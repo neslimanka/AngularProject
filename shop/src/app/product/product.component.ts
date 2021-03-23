a7ff7ae77075b68d477ef0b9e2f6ceb1a3c0c770
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-03-23 22:22:21 +0300

    Ürünleri listeleme

--- shop/src/app/product/product.component.ts
+++ shop/src/app/product/product.component.ts
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   constructor() { }
+  title="Ürün Listesi";
+  products : any []=[
+    {id:1,name:"Laptop",price:2500,categoty:1,description:"Asus ZenBook"},
+    {id:2,name:"Laptop",price:25,categoty:2,description:"A4 Tech"}
+
+  ]
 
   ngOnInit(): void {
   }
