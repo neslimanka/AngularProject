bda9442f2d0e9929e2b464bf0ca32179d20ece56
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-03-25 21:20:24 +0300

    ngFor

--- shop/src/app/product/product.component.ts
+++ shop/src/app/product/product.component.ts
@@ -8,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   constructor() { }
-  title="Ürün Listesi"
-  products : any[] | undefined;
+  title="Ürün Listesi";
+  products : any []=[
+    {id:1,name:"Laptop",price:2500,categoty:1,description:"Asus ZenBook"},
+    {id:2,name:"Laptop",price:25,categoty:2,description:"A4 Tech"},
+    {id:1,name:"Laptop",price:2500,categoty:1,description:"Asus ZenBook"},
+    {id:1,name:"Laptop",price:2500,categoty:1,description:"Asus ZenBook"},
+    {id:1,name:"Laptop",price:2500,categoty:1,description:"Asus ZenBook"},
+    {id:1,name:"Laptop",price:2500,categoty:1,description:"Asus ZenBook"},
+
+  ]
 
   ngOnInit(): void {
   }
